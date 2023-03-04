4739e11898c84726ae20d48d79ab39dba5fa682b
HanSeongLee <jkhanseong@gmail.com>
2023-03-04 19:48:16 +0900

    ADD | Form component

---
+++ components/Form/index.tsx
@@ -0,0 +1,68 @@
+import React, { FormHTMLAttributes } from 'react';
+import styles from './style.module.scss';
+import cn from 'classnames';
+import Button from 'components/Button';
+
+interface IProps extends FormHTMLAttributes<HTMLFormElement> {
+    variant?: 'primary' | 'danger' | 'none';
+    title: string;
+    description?: string;
+    buttonName: string;
+    onCancel?: () => void;
+}
+
+const Form: React.FC<IProps> = ({
+                                    variant, title, description, buttonName,
+                                    onCancel, className, children, ...props
+                                }) => {
+    return (
+        <form className={cn(styles.form, {
+            [styles.danger]: variant === 'danger',
+        }, className)}
+              {...props}
+        >
+            <h3 className={styles.title}>
+                {title}
+            </h3>
+            {description && (
+                <p className={styles.description}>
+                    {description}
+                </p>
+            )}
+
+            {children && (
+                <div className={styles.container}>
+                    {children}
+                </div>
+            )}
+
+            <div className={styles.buttonContainer}>
+                {variant === 'primary' && (
+                    <Button type={'submit'}>
+                        {buttonName}
+                    </Button>
+                )}
+                {variant === 'danger' && (
+                    <>
+                        <Button type={'submit'}
+                                variant={'danger'}
+                        >
+                            {buttonName}
+                        </Button>
+                        <Button variant={'secondary'}
+                                onClick={onCancel}
+                        >
+                            Cancel
+                        </Button>
+                    </>
+                )}
+            </div>
+        </form>
+    );
+};
+
+Form.defaultProps = {
+    variant: 'primary',
+};
+
+export default Form;
