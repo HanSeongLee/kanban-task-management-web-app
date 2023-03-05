2379ba42ba361abe84e84f2a2c66655ac994aa84
HanSeongLee <jkhanseong@gmail.com>
2023-03-05 18:22:03 +0900

    ADD | AddNewTaskFormContainer

---
+++ containers/AddNewTaskFormContainer/index.tsx
@@ -0,0 +1,73 @@
+import React, { HTMLAttributes, useMemo } from 'react';
+import { useForm } from 'react-hook-form';
+import TaskForm from 'components/TaskForm';
+import { useAppStore } from 'lib/store';
+import { useRouter } from 'next/router';
+
+interface IProps extends HTMLAttributes<HTMLDivElement> {
+
+}
+
+const AddNewTaskFormContainer: React.FC<IProps> = (props) => {
+    const router = useRouter();
+    const { query: { id } } = router;
+    const { boards, addTask, closeModal } = useAppStore();
+    const currentBoard = useMemo(() => {
+        return boards.find(({ id: _id }) => _id === Number(id));
+    }, [id, boards]);
+    const status: Option[] = currentBoard?.columns.map(({ id, name }) => {
+        return {
+            label: name,
+            value: `${id}`,
+        };
+    });
+    const defaultValues = {
+        title: '',
+        description: '',
+        subtasks: [
+            {
+                value: '',
+            },
+            {
+                value: '',
+            },
+        ],
+        status: status[0].value,
+    };
+    const { control, handleSubmit, formState: { errors } } = useForm({
+        defaultValues,
+    });
+
+    const onSubmit = (data: object) => {
+        const { title, description, subtasks, status } = data;
+        const newSubtasks: Subtask[] = subtasks.map(({ value }, index) => {
+            return {
+                id: index + 1,
+                title: value,
+                isCompleted: false,
+            };
+        });
+        const newTask: Task = {
+            id: -1,
+            title,
+            description,
+            status,
+            subtasks: newSubtasks,
+        };
+        addTask(Number(id), newTask);
+        closeModal();
+    };
+
+    return (
+        <TaskForm title={'Add New Task'}
+                  buttonName={'Create Task'}
+                  control={control}
+                  errors={errors}
+                  onSubmit={handleSubmit(onSubmit)}
+                  status={status}
+                  {...props}
+        />
+    );
+};
+
+export default AddNewTaskFormContainer;
