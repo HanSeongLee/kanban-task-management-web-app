8e4bdb6cbb8224c3d1dafdc96194e537034877a7
HanSeongLee <jkhanseong@gmail.com>
2023-03-09 19:14:21 +0900

    ADD | Functions of BoardOptionButtonContainer

--- containers/BoardOptionButtonContainer/index.tsx
+++ containers/BoardOptionButtonContainer/index.tsx
@@ -1,39 +1,63 @@
-import React, { HTMLAttributes } from 'react';
+import React, { HTMLAttributes, useMemo, useState } from 'react';
 import DropdownButton from 'components/DropdownButton';
 import Button from 'components/Button';
+import { useAppStore } from 'lib/store';
+import { useRouter } from 'next/router';
 
 interface IProps extends HTMLAttributes<HTMLDivElement> {
 
 }
 
 const BoardOptionButtonContainer: React.FC<IProps> = (props) => {
+    const [openMenu, setOpenMenu] = useState<boolean>(false);
+    const { boards, openDeleteBoardModal, openEditBoardModal, openAddNewTaskModal } = useAppStore();
+    const router = useRouter();
+    const { query: { id } } = router;
+    const currentBoard = useMemo(() => {
+        return boards.find((({ id: _id }) => _id === Number(id)));
+    }, [id, boards]);
+
+    const toggleOpenMenu = () => {
+        setOpenMenu(!openMenu);
+    };
+
     const onAddNewTask = () => {
-        // TODO: implement here.
+        openAddNewTaskModal();
     };
 
     const onEditBoard = () => {
-        // TODO: implement Here.
+        openEditBoardModal();
+        toggleOpenMenu();
     };
 
     const onDeleteBoard = () => {
-        // TODO: implement Here.
+        if (boards.length === 1) {
+            return;
+        }
+
+        openDeleteBoardModal();
+        toggleOpenMenu();
     };
 
     return (
         <div {...props}>
             <Button icon={'/icons/icon-add-task-mobile.svg'}
                     round
-                    disabled
+                    disabled={currentBoard?.columns?.length === 0}
                     onClick={onAddNewTask}
             >
                 + Add New Task
             </Button>
 
-            <DropdownButton icon={'/icons/icon-vertical-ellipsis.svg'}>
+            <DropdownButton open={openMenu}
+                            toggleOpen={toggleOpenMenu}
+                            icon={'/icons/icon-vertical-ellipsis.svg'}
+            >
                 <li onClick={onEditBoard}>
                     Edit Board
                 </li>
                 <li onClick={onDeleteBoard}
+                    aria-disabled={boards.length === 1}
                     style={{
                         color: 'var(--theme-danger-color)'
                     }}
