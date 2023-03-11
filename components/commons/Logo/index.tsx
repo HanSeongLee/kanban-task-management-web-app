1c063362b1cc7d80bda307625f27fc0243a16b69
HanSeongLee <jkhanseong@gmail.com>
2023-03-11 15:33:13 +0900

    ADD | Logo component on tablet

--- components/commons/Logo/index.tsx
+++ components/commons/Logo/index.tsx
@@ -1,22 +1,32 @@
-import React, { AnchorHTMLAttributes } from 'react';
-import styles from 'components/commons/Logo/style.module.scss';
+import React, { AnchorHTMLAttributes, useContext } from 'react';
+import styles from './style.module.scss';
 import Link from 'next/link';
 import cn from 'classnames';
+import { ThemeContext } from 'lib/ThemeContext';
+import { Theme } from 'types/theme';
 
 interface IProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
 
 }
 
 const Logo: React.FC<IProps> = ({ className, ...props }) => {
+    const { theme } = useContext(ThemeContext);
+
     return (
         <Link href={'/'}>
             <a className={cn(styles.logo, className)}
                {...props}
             >
                 <h1>
-                    <img src={'/logo-mobile.svg'}
-                         alt={'kanban'}
-                    />
+                    <picture>
+                        <source srcSet={theme === Theme.LIGHT ? '/logo-dark.svg' : '/logo-light.svg'}
+                                media={'(min-width: 568px)'}
+                        />
+
+                        <img src={'/logo-mobile.svg'}
+                             alt={'kanban'}
+                        />
+                    </picture>
                 </h1>
             </a>
         </Link>
