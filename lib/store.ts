fc99953ee9bad9909efa82cc9e88b065adb1b4ad
HanSeongLee <jkhanseong@gmail.com>
2023-03-13 20:13:05 +0900

    ADD | Persist middleware
    Signed-off-by: HanSeongLee <jkhanseong@gmail.com>

--- lib/store.ts
+++ lib/store.ts
@@ -2,11 +2,16 @@ import { create } from 'zustand';
 import { createSidebarSlice, SidebarSlice } from 'lib/slices/createSidebarSlice';
 import { BoardSlice, createBoardSlice } from 'lib/slices/createBoardSlice';
 import { createModalSlice, ModalSlice } from 'lib/slices/createModalSlice';
+import { persist } from 'zustand/middleware';
 
 type StoreState = ModalSlice & BoardSlice & SidebarSlice;
 
-export const useAppStore = create<StoreState>()((...a) => ({
-    ...createModalSlice(...a),
-    ...createBoardSlice(...a),
-    ...createSidebarSlice(...a),
-}));
+export const useAppStore = create<StoreState>()(
+    persist((...a) => ({
+        ...createModalSlice(...a),
+        ...createBoardSlice(...a),
+        ...createSidebarSlice(...a),
+    }), {
+        name: 'kanban-task-management-store',
+    })
+);
